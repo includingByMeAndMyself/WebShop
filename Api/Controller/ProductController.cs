4335976b2d7f0b63d6352b58f26cb8e888b33f05
IvanSmirnov <>
2025-08-20 21:55:39 +0500

    Доделал ProductController

--- Api/Controller/ProductController.cs
+++ Api/Controller/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using Api.Data;
 using Api.Model;
+using Api.ModelDto;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -41,5 +42,96 @@ public class ProductController : StoreController
         response.Result = product;
 
         return Ok(response);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ResponseServer>> CreateProduct(
+        [FromBody] ProductCreateDto product)
+    {
+        Product item = new Product()
+        {
+            Name = product.Name,
+            Descritopr = product.Descritopr,
+            Category = product.Category,
+            Image = product.Image,
+            Price = product.Price
+        };
+
+        await dbContext.AddAsync(item);
+        await dbContext.SaveChangesAsync();
+
+        return Created();
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<ResponseServer>> UpdateProduct(
+        [FromBody] Guid id, ProductUpdateDto product)
+    {
+        var productFromDb = await dbContext.Products.FindAsync(id);
+        if (productFromDb is null)
+        {
+            return NotFound(new ResponseServer
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.NotFound
+            });
+        }
+
+        productFromDb.Name = product.Name;
+        productFromDb.Descritopr = product.Descritopr;
+        productFromDb.Category = product.Category;
+        productFromDb.Image = product.Image;
+        productFromDb.Price = product.Price;
+
+        dbContext.Products.Update(productFromDb);
+        await dbContext.SaveChangesAsync();
+
+        return Ok();
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<ResponseServer>> DeleteProduct(
+        [FromBody] Guid id)
+    {
+        try
+        {
+            if (id != null)
+            {
+                return BadRequest(new ResponseServer()
+                {
+                    IsSuccess = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Невалидный Id" }
+                });
+            }
+
+            var productFromDb = await dbContext.Products.FindAsync(id);
+            if (productFromDb is null)
+            {
+                return NotFound(new ResponseServer()
+                {
+                    IsSuccess = false,
+                    StatusCode = HttpStatusCode.NotFound,
+                    ErrorMessages = { "Продукт по id не найден" }
+                });
+            }
+
+            dbContext.Products.Remove(productFromDb);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(new ResponseServer()
+            {
+                StatusCode = HttpStatusCode.NoContent
+            });
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new ResponseServer()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { e.Message }
+            });
+        }
     }
 }
---
+++ Api/ModelDto/ProductUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.ModelDto;
+
+public class ProductUpdateDto
+{
+    [Required]
+    public string Name { get; set; }
+    public string Descritopr { get; set; }
+    public string SpecialTag { get; set; }
+    public string Category { get; set; }
+    public string Image { get; set; }
+    public decimal Price { get; set; }
+}
